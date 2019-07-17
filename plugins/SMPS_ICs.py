aa5f31351d17ba77bae3bae8be42652ee91dbd68
Johannes Bauer <joe@johannes-bauer.com>
2019-07-17 22:40:42 +0200

    Fix switched R1/R2 for some SMPS ICs
    For some ICs, R1 is the low side while for others it is the high side of
    the feedback voltage divider. Take it into account and show a proper
    warning.

--- plugins/SMPS_ICs.py
+++ plugins/SMPS_ICs.py
@@ -39,6 +39,10 @@ _form_template = """
 """
 
 _response_template = """
+%if d["r1_r2_switched"]:
+<div class="warning">Warning! In our calculations, R<sub>1</sub> is always the low side. This is exactly the opposite as it is in the datasheet of this device (i.e., identifiers R<sub>1</sub> and R<sub>2</sub> are switched).</div>
+%endif
+
 ${result_table_begin("R<sub>1<sub>", "R<sub>2</sub>", "V<sub>out</sub>", "Error")}
 
 %for option in d["options"]:
@@ -59,6 +63,26 @@ class Plugin(BasePlugin):
 	_MENU_HIERARCHY = ("SMPS", "SMPS ICs")
 	_FORM_TEMPLATE = _form_template
 	_RESPONSE_TEMPLATE = _response_template
+	_VOUT = {
+		"lm2596-25degc":	(1.2, 37),
+		"lm2596-full":		(1.2, 37),
+		"xl4005":			(0.8, 30),
+		"xl4015":			(1.25, 32),
+		"mp1584":			(0.8, 25),
+		"mp2307":			(0.925, 20),
+#		"ap3502":			(),		# Unspecified?
+		"cx8509":			(0.925, 20),
+	}
+
+	# By deafult, R1 is the low side (connects to GND). For some ICs, it's the
+	# exact opposite. For us, R1 is *always* the low side, i.e., for some chips
+	# we might want to show a warning they're switched.
+	_R1_R2_SWITCHED = {
+		"mp1584":			True,
+		"mp2307":			True,
+		"ap3502":			True,
+		"cx8509":			True,
+	}
 	_VREFS = {
 		"lm2596-25degc":	(1.193, 1.23, 1.267),
 		"lm2596-full":		(1.18, 1.23, 1.28),
@@ -66,52 +90,61 @@ class Plugin(BasePlugin):
 		"xl4015":			(1.225, 1.25, 1.275),
 		"mp1584":			(0.776, 0.8, 0.824),
 		"mp2307":			(0.900, 0.925, 0.950),
+		"ap3502":			(0.907, 0.925, 0.943),
+		"cx8509":			(0.900, 0.925, 0.950),
 	}
 	_R1_RANGE = {
 		"lm2596-25degc":	(240, 1500),
 		"lm2596-full":		(240, 1500),
 		"xl4005":			(2e3 * 0.75, 2e3 * 1.25),
 		"xl4015":			(240, 1500),
-		"mp1584":			(240, 8000),	# R2 < 40k
-		"mp2307":			(240, 4000),	# R2 < 100k
-	}
-	_R2_MAX = {
-		"mp1584":			40000,
-		"mp2307":			100000,
+		"mp1584":			(1000, 40000),		# R1 < 40k
+		"mp2307":			(4700, 100000),		# R1 < 100k, ideally 10k
+#		"ap3502":			(),
+		"cx8509":			(4700, 100000),		# R1 < 100k, ideally 10k
 	}
 
+	def _get_vout(self, ic_name, vref, r1, r2):
+		return vref * (1 + (r2 / r1))
+
+	def _get_r2(self, ic_name, vref, r1, v_out):
+		return r1 * ((v_out / vref) - 1)
+
 	def request(self, endpoint, parameters):
+		part = parameters["part"]
 		v_out = UnitValue(parameters["v_out"])
 		r_set = self.config.get_valuesets("r")[parameters["r_set"]]
-		(v_ref_min, v_ref_typ, v_ref_max) = self._VREFS[parameters["part"]]
-		(r1_rangemin, r1_rangemax) = self._R1_RANGE[parameters["part"]]
-		r2_abs_max = self._R2_MAX.get(parameters["part"])
+		(v_ref_min, v_ref_typ, v_ref_max) = self._VREFS[part]
+		(r1_rangemin, r1_rangemax) = self._R1_RANGE[part]
 		try:
 			r_tolerance = float(parameters["r_tolerance"]) / 100
 		except ValueError:
 			r_tolerance = 0
+
+		# Low side R1:
 		# vout = vref * (1 + (r2 / r1))
 		# vout / vref = 1 + (r2 / r1)
 		# (vout / vref) - 1 = r2 / r1
 		# r2 = r1 * ((vout / vref) - 1)
 		# r1 = r2 / ((vout / vref) - 1)
 
+		# Low side R2:
+		# r2 = r1 / ((vout / vref) - 1)
+
 		options = [ ]
 		for r1 in r_set.iter_range(r1_rangemin, r1_rangemax):
-			ideal_r2 = float(r1) * ((float(v_out) / v_ref_typ) - 1)
+			ideal_r2 = self._get_r2(ic_name = part, vref = v_ref_typ, r1 = float(r1), v_out = float(v_out))
 			for r2 in r_set.iter_closest(ideal_r2):
-				if (r2_abs_max is not None) and (float(r2) > r2_abs_max):
-					# Invalid combination, R2 is too large
-					continue
-
 				r1_min = float(r1) * (1 - r_tolerance)
 				r1_max = float(r1) * (1 + r_tolerance)
 				r2_min = float(r2) * (1 - r_tolerance)
 				r2_max = float(r2) * (1 + r_tolerance)
 
-				typical_v_out = v_ref_typ * (1 + (float(r2) / float(r1)))
-				minimal_v_out = v_ref_min * (1 + (r2_min / r1_max))
-				maximal_v_out = v_ref_max * (1 + (r2_max / r1_min))
+				typical_v_out = self._get_vout(ic_name = part, vref = v_ref_typ, r1 = float(r1), r2 = float(r2))
+				extreme1_v_out = self._get_vout(ic_name = part, vref = v_ref_typ, r1 = r1_min, r2 = r2_max)
+				extreme2_v_out = self._get_vout(ic_name = part, vref = v_ref_typ, r1 = r1_max, r2 = r2_min)
+				minimal_v_out = min(extreme1_v_out, extreme2_v_out)
+				maximal_v_out = max(extreme1_v_out, extreme2_v_out)
 
 				error_min_v_out = (minimal_v_out - float(v_out)) / float(v_out)
 				error_max_v_out = (maximal_v_out - float(v_out)) / float(v_out)
@@ -129,10 +162,30 @@ class Plugin(BasePlugin):
 				options.append(option)
 		options.sort(key = lambda opt: abs(opt["error_typical"]))
 		return {
-			"options":		options[ : 15 ],
+			"r1_r2_switched":		self._R1_R2_SWITCHED.get(part, False),
+			"options":				options[ : 15 ],
 		}
 
 if __name__ == "__main__":
 	from pyengineer import Configuration
 	plugin = Plugin(Configuration("configuration.json"), instanciated_from = __file__)
 	plugin.dump_request({ "v_out": "3.3", "r_set": "E12", "part": "lm2596-full", "r_tolerance": "5" })
+
+	example_values = [
+		("lm2596-full", 1e3, 8756, 12),
+		("lm2596-25degc", 1e3, 8756, 12),
+		("xl4005", 2e3, 28e3, 12),
+		("xl4015", 3.3e3, 10e3, 5),
+		("mp1584", 40.2e3, 124e3, 3.3),
+		("mp2307", 10e3, 26.1e3, 3.3),
+#		("ap3502", 10e3, 26.1e3, 3.3),
+		("cx8509", 10e3, 44.2e3, 5),
+	]
+	for (chipname, r1, r2, output_voltage) in example_values:
+		result = plugin.request(None, { "v_out": str(output_voltage), "r_set": "E24", "part": chipname, "r_tolerance": "0" })
+		best = result["options"][0]
+		assert(abs(best["v_out_typical"]["flt"] - output_voltage) < 0.25)
+
+		best_ratio = best["r1"]["flt"] / best["r2"]["flt"]
+		ratio = r1 / r2
+		assert(abs((ratio - best_ratio) / best_ratio) <= 0.05)
